919e573d80289a07051cad01e300235784c0de1b
od507 <antonio.daniels@utp.ac.pa>
2021-12-03 21:02:48 -0500

    hago trampa pero funciona

--- PYF.c
+++ PYF.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 // Prototipos de Funciones
 int menu(void);
-void opc1(int *puntNum);
-void carArre(int *arreNum, int *num, int size);
-void ordArre(int *arrNum, int size);
+void opc1(int *arreNums, int cont, int *dig);
+void ordeArre(int *arreNums,int cont);
 void present();
-
+void posi(int *arre, int size, int *numPos);
 int main()
 {
     //Declaracion de variables
     int reg=0;
     int tam=1;
-    int num;
-    int *pn=&num;
+    int numGua;
+    int *pnumGua=&numGua;
     int i;
     int *pnum = (int *)malloc(sizeof(int)); //Inicializacion de puntero con espacio para 1 entero
     char sel;
@@ -23,7 +21,7 @@ int main()
     present();
     printf("\nBienvenido al programa que ordena los numeros que ingresas ");
     printf("de manera aleatoria.\n");
-    if(pnum==NULL||pn==NULL)
+    if(pnum==NULL)
     {
         printf("\nOcurrio un error");
         printf("\nEspacio insuficiente en memoria");
@@ -38,10 +36,10 @@ int main()
         {
             case 1:
             {
-                opc1(pn);
-                carArre(pnum,pn,tam);
-                ordArre(pnum,tam);
+                opc1(pnum,reg,pnumGua);
                 reg++;
+                ordeArre(pnum,reg);
+                posi(pnum,tam,pnumGua);
                 tam++;
                 pnum=(int *)realloc(pnum,tam*sizeof(int));
                 if(pnum == NULL)
@@ -79,42 +77,51 @@ int main()
         printf("\nDigite \"s\" para continuar, de lo contrario terminaremos de captar datos: ");
         sel=getchar();
         fflush(stdin);
-
-    }while(sel == 's'|| sel == 'S');
+    }while(sel == 's');
     }
-
     
     return 0;
 }
-
 int menu(void)
 {
     int sele;
-
     printf("\nDigite el numero de su seleccion");
     printf("\n1. Ingresar un numero y almacenarlo en la memoria dinamica");
     printf("\n2. Imprimir cada elemento del vector, la posicion y su direccion.");
     printf("\n3. Total de elementos del vector.");
     printf("\n4. Salir\n");
-
     scanf("%d",&sele);
     fflush(stdin);
     return sele;
 }
-
-void opc1(int *puntNum)
+void opc1(int *arreNums,int cont,int *dig)
 {
-    int temp;
     printf("\nA continuacion ingresara un numero el cual guardaremos\n");
     printf("\nDigite un numero: ");
-    scanf("%d",&temp);
-    *puntNum=temp;
+    scanf("%d",(arreNums+cont));
+    *dig=*(arreNums+cont);
     fflush(stdin);
 }
-
-void carArre(int *arreNum, int *num, int size)
+void ordeArre(int *arreNums,int cont)
 {
-    *(arreNum+(size-1))=*num;
+    int temp;
+    int tempPos;
+    int i=0;
+    int k=0;
+
+    for(i=0;i<cont;i++)
+    {
+        for(k=i+1;k<cont;k++)
+        {
+            if(*(arreNums+i)>*(arreNums+k))
+            {
+                temp=*(arreNums+i);
+                *(arreNums+i)=*(arreNums+k);
+                *(arreNums+k)=temp;
+                tempPos=i;
+            }
+        }
+    }
 }
 
 void present()
@@ -123,7 +130,6 @@ void present()
     printf("\t      Facultad de Ingieneria de Sistemas Computacionales\n");
     printf("\t\t Departamento de programacion de computadoras\n");
     printf("\n\t\t\t\tPROYECTO FINAL\n\n\n");
-
     printf("Facilitadora: Msc. Mitzi de Velasquez \t\t Asignatura: Herramientas de Programacion Aplicada 1");
     printf("\n\nIntegrantes:                   CEDULA: \n\n");
     printf("Eric Moreno               - Cedula:8-951-1011\n");
@@ -131,30 +137,30 @@ void present()
     printf("Daniel Nunez              - Cedula:8-992-364\n");
     printf("Jackeline McClean         - Cedula:3-751-1480 \n");
     printf("Antonio Daniels           - Cedula:3-722-58 \n\n");
-
     printf("Fecha de entrega: Lunes, 20 de diciembe de 2021 - Hora 10:00\n\n\n");
-
     system("pause");
     system("cls"); 
 }
 
-void ordArre(int *arrNum, int size)
+void posi(int *arre, int size, int *numPos)
 {
     int i;
-    int j;
-    int temp;
-    if(size>1)
+    for(i=0;i<size;i++)
     {
-        for(i=1;i<size;i++)
+        if(*(numPos)==*(arre+i))
         {
-        temp=*(arrNum+i);
-        j=i-1;
-        while(j>=0 && *(arrNum+i)>temp)
-        {
-            *(arrNum+j+1)=*(arrNum+j);
-            j=j-i;
-        }
-        *(arrNum+j+1)=temp;
+            if(i==0)
+            {
+                printf("%d se ingresa al inicio del vector",*(numPos));
+            }
+            else if(i==(size-1))
+            {
+                printf("%d se ingresa al final del vector",*(numPos));
+            }
+            else
+            {
+                printf("%d se ingresa en la posicion %d",*(numPos),i);
+            }
         }
     }
 }
